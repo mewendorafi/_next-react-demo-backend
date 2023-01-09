cb3e58f1b974329756b7578704f785eec0c06c12
Méwen DORAFI <dorafimewen@pm.me>
2023-01-09 15:38:20 +0100

    :recycle: :sparkles: Switched to async/await syntax, added /login route

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,21 @@ const router = express.Router()
 const fetch = require('node-fetch')
 
 // GET users from JSONplaceholder API
-router.get('/', function (req, res, next) {
-	fetch('https://jsonplaceholder.typicode.com/users')
-		.then(res => res.json())
-		.then(users => {
-			console.log(users)
-			res.json({ users })
-		})
+router.get('/', async function (req, res, next) {
+	const rawRes = await fetch('https://jsonplaceholder.typicode.com/users')
+	const jsonRes = await rawRes.json()
+	jsonRes ? console.log(jsonRes) : res.json({ success: false, users: null })
+	res.json({ success: true, users: jsonRes })
+})
+
+//POST user login
+router.post('/login', function (req, res, next) {
+	console.log(req.body)
+	let result = false
+	let user = null
+	const { username, email, password } = req.body
+	if (username && email && password) (result = true), (user = { username, email, password })
+	res.json({ success: result, user })
 })
 
 module.exports = router
